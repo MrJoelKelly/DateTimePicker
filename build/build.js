d6f233c0a7b14d909c302690a3cab004fd5e3401
Joel Kelly <joel.kelly@keltec.systems>
2016-12-01 19:42:02 +0100

    Added time section to design

--- build/build.js
+++ build/build.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  
+  $('article.design div.DateTimePicker div.button-calendar').click(function(){
+    $('article.design article.time').stop().slideUp(200);
+    $('article.design article.calendar').stop().slideDown(200);
+  })
+  $('article.design div.DateTimePicker div.button-time').click(function(){
+    $('article.design article.calendar').stop().slideUp(200);
+    $('article.design article.time').stop().slideDown(200);
+  })
+
+  $('div.calendar').dateTimePicker({lang:'en'});
 })
